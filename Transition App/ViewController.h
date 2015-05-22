329dd316c00a4f9964d1eb1a2c90d16a9f379f87
Richard Szczerba <richardszczerba@gmail.com>
2015-05-22 19:16:28 +0100

    Initial Commit

---
+++ Transition App/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Transition App
+//
+//  Created by Richard Szczerba on 22/05/15.
+//  Copyright (c) 2015 Richard Szczerba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Transition App/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Transition App
+//
+//  Created by Richard Szczerba on 22/05/15.
+//  Copyright (c) 2015 Richard Szczerba. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
